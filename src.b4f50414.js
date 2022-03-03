16429f7814913b956ea351d10eacd85b8f678a3b
Vidiborets <45356315+Vidiborets@users.noreply.github.com>
2022-03-03 09:20:51 +0000

    Deploying to gh-pages from @ Vidiborets/resume@f264d5deb33d6733d218ca24c6dcb28498163b9d 🚀

--- src.b4f50414.js
+++ src.b4f50414.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/resume/src.3c7e5439.js.map
+//# sourceMappingURL=/resume/src.b4f50414.js.map
